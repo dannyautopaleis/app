64527754ab95bfcade9cea3452d8d3c2005e3a0b
z3ntl3 <z3ntl3discord@gmail.com>
2025-09-24 17:52:42 +0200

    re-export svg paths from one module

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,16 +4,18 @@ import { View, Pressable } from "react-native";
 import { type BottomTabBarProps } from "@react-navigation/bottom-tabs";
 
 // icons imports --------------- 
-import Home from '../../../assets/svgs/home.svg';
-import Home_dark from '../../../assets/svgs/home_dark.svg';
-import Products from '../../../assets/svgs/products.svg';
-import Products_dark from '../../../assets/svgs/products_dark.svg';
-import Lendings from '../../../assets/svgs/repair.svg';
-import Lendings_dark from '../../../assets/svgs/repair_dark.svg';
-import Notifications from '../../../assets/svgs/notifications.svg';
-import Notifications_dark from '../../../assets/svgs/notifications_dark.svg';
-import Settings from '../../../assets/svgs/gear.svg';
-import Settings_dark from '../../../assets/svgs/gear_dark.svg';
+import { 
+    Home,
+    Home_dark,
+    Products,
+    Products_dark,
+    Lendings,
+    Lendings_dark,
+    Notifications,
+    Notifications_dark,
+    Settings,
+    Settings_dark
+ } from "../../../@types/svg_reexports";
 // ------------end imports
 
 type ColorScheme = "dark" | "light"
