6fa3abd1ce56c580c7f66ee9cac05222025517a2
z3ntl3 <z3ntl3discord@gmail.com>
2025-10-16 09:33:15 +0200

    device token setup for push notifications

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,20 @@
-import { JSX } from "react";
+import { JSX, useState } from "react";
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import * as Notifications from 'expo-notifications';
 
 export default function RootStackLayout(): JSX.Element {
+    let [deviceToken, setDeviceToken] = useState<string>()
+    Notifications.getDevicePushTokenAsync()
+        .then((v) => {
+            setDeviceToken(v.data)
+            console.log("Notifications push token:",v.data)
+        })
+        .catch((err) => {
+            console.log("failed retrieving device push token")
+        })
+
     return (
         <SafeAreaProvider>
             <StatusBar style="dark" />
