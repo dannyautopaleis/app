e42f240ac1c97ead6e7535d3ce86d4f88bd47d57
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2025-11-05 11:27:11 +0100

    11/05/2025 11:27:10

--- src/app/auth/(tabs)/index.tsx
+++ src/app/auth/(tabs)/index.tsx
@@ -1,14 +1,9 @@
 import { JSX, useContext } from "react";
 import {
-  Animated,
-  Easing,
-  FlatList,
-  NativeScrollEvent,
-  NativeSyntheticEvent,
   Platform,
   StyleProp,
   Text,
-  TouchableOpacity,
+  FlatList,
   View,
   ViewStyle,
 } from "react-native";
@@ -17,6 +12,7 @@ import { useRoute } from "@react-navigation/native";
 import { useRouter } from "expo-router";
 import { Image } from "expo-image";
 import { DynamicHeaderProvider } from "@/src/contexts/DynamicHeaderProvider";
+import { FlashList } from "@shopify/flash-list"; // we'll use flashlist upon production, its fully compatible with flatlist so we do not mind speeding the process
 
 // this is used for testing
 const staticItems = [
@@ -47,98 +43,103 @@ export default function HomeScreen(): JSX.Element {
     >
       <View style={{marginTop: 5}}/>
       <FlatList 
-      bounces={false}
-      showsVerticalScrollIndicator={false}
-      directionalLockEnabled={true}
-      onScroll={header.scrollBar}
-      numColumns={2}
-      snapToAlignment="start"
-      snapToInterval={30}
-      scrollToOverflowEnabled={false}
-      contentContainerStyle={{
-        width: "100%",
-        // borderColor: "red",
-        // borderWidth: 1,
-      }} data={staticItems} renderItem={({item}) => {
-        return (
-          <View style={{
-            display: "flex",
-            alignItems: "center",
-            backgroundColor: "#FFFFFF",
-            width: 180,
-            height: 160,
-            margin: 7,
-            borderRadius: 2,
-            boxShadow: "4px 4px 100px 1px rgba(0, 0, 0, 0.05)"
-          }}>
-            <Image style={{padding: 0, width: "100%", height: "75%", backgroundColor: "red"}} source={item.image} />
-            <View style={{backgroundColor: "#282827", width: "100%", height: 1.5}}/>
-
+        style={{
+          width: "100%",
+          height: "auto"
+        }}
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        directionalLockEnabled={true}
+        onScroll={header.scrollBar}
+        numColumns={2}
+        snapToAlignment="start"
+        snapToInterval={30}
+        scrollToOverflowEnabled={false}
+        contentContainerStyle={{
+          paddingHorizontal: 10
+        }} 
+        data={staticItems} 
+        renderItem={(ctx) => {
+          const { item } = ctx
+          return (
             <View style={{
               display: "flex",
-              flexDirection: "row",
-              alignSelf: "flex-start",
-              padding: 5,
-              paddingHorizontal: 8,
-              flex: 1
+              alignItems: "center",
+              backgroundColor: "#FFFFFF",
+              width: 180,
+              height: 160,
+              margin: 5,
+              borderRadius: 2,
+              boxShadow: "4px 4px 100px 1px rgba(0, 0, 0, 0.05)"
             }}>
-              <Text style={{
-                fontFamily: Platform.select({
-                  ios: "Barlow Bold",
-                  android: "Barlow_700Bold"
-                }), 
-                fontWeight: 700
-              }}>{item.title}</Text>
+              <Image style={{padding: 0, width: "100%", height: "75%"}} source={item.image} />
+              <View style={{backgroundColor: "#282827", width: "100%", height: 1.5}}/>
+
               <View style={{
                 display: "flex",
                 flexDirection: "row",
-                flexGrow: 1,
-                justifyContent: "flex-end",
-                alignItems: "center"
+                alignSelf: "flex-start",
+                padding: 5,
+                paddingHorizontal: 8,
+                flex: 1
               }}>
-                
                 <Text style={{
                   fontFamily: Platform.select({
-                    ios: "Barlow Regular",
-                    android: "Barlow_400Regular"
-                  }),
-                  fontSize: 14,
-                  marginRight: 2
-                }}>{item.price}</Text>
-                {/* <Image source={require('@/assets/img/coin.png')} 
-                  style={{width: 18, height: 18}}
-                /> */}
+                    ios: "Barlow Bold",
+                    android: "Barlow_700Bold"
+                  }), 
+                  fontWeight: 700
+                }}>{item.title}</Text>
                 <View style={{
                   display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  backgroundColor: "#efbc3cff",
-                  width: 23,
-                  height: 23,
-                  borderRadius: 30,
-                  borderColor: "#282827",
-                  borderWidth: 1,
-                  padding: 0.1
+                  flexDirection: "row",
+                  flexGrow: 1,
+                  justifyContent: "flex-end",
+                  alignItems: "center"
                 }}>
+                  
+                  <Text style={{
+                    fontFamily: Platform.select({
+                      ios: "Barlow Regular",
+                      android: "Barlow_400Regular"
+                    }),
+                    fontSize: 14,
+                    marginRight: 2
+                  }}>{item.price}</Text>
+                  {/* <Image source={require('@/assets/img/coin.png')} 
+                    style={{width: 18, height: 18}}
+                  /> */}
                   <View style={{
-                    borderRadius: 30,
-                    borderColor: "#282827",
-                    borderWidth: 1,
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
-                    width: "90%",
-                    height: "90%"
+                    backgroundColor: "#efbc3cff",
+                    width: 23,
+                    height: 23,
+                    borderRadius: 30,
+                    borderColor: "#282827",
+                    borderWidth: 1,
+                    padding: 0.1
                   }}>
-                    <Text style={{
-                      fontSize: 10
-                    }}>$</Text>
+                    <View style={{
+                      borderRadius: 30,
+                      borderColor: "#282827",
+                      borderWidth: 1,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: "90%",
+                      height: "90%"
+                    }}>
+                      <Text style={{
+                        fontSize: 10
+                      }}>$</Text>
+                    </View>
                   </View>
                 </View>
               </View>
             </View>
-          </View>
-        )
+          )
       }}/>
     </SafeAreaView>
   );
