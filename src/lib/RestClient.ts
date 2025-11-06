fc22502ca8728518ebd9cda7a5bbaa37aba2abed
z3ntl3 <root@z3ntl3.com>
2025-11-06 22:55:04 +0100

    add restclient with some unfinished work

---
+++ src/lib/RestClient.ts
@@ -0,0 +1,16 @@
+export interface Filter {
+    search: string
+    category: string
+    order: "desc" | "asc"
+}
+
+export var RestBaseURL = "https://school.z3ntl3.com/api/v1/tools"
+export class RestClient {
+    private jwt: string
+
+    constructor(jwt: string) {
+        this.jwt = jwt
+    }
+
+    getProducts(page: number, entries: number, filter?: Filter) {}
+}
