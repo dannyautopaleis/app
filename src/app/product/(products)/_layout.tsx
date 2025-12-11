6cf8ea4813aa4b70298bc6ea2652f40742bc58c0
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2025-12-11 12:08:53 +0100

    componentize new header

--- src/app/product/(products)/_layout.tsx
+++ src/app/product/(products)/_layout.tsx
@@ -4,6 +4,7 @@ import { Pressable, View, Text, Platform} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 
 export default function() {
     return <>
@@ -13,75 +14,45 @@ export default function() {
                 header: Header
             }} /> 
             <Stack.Screen  name="create"  options={{
-                header: (props) => (
-                    <SafeAreaView edges={["left", "right", "top"]} style={{
-                        width: "100%",
-                        height: 100,
-                        backgroundColor: "#FFEE49",
-                        paddingHorizontal: 20,
-                        
-                    }}>
-                        <Pressable style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            alignItems: "center",
-                            flex: 1
-                        }} onPress={() => props.navigation.goBack()}>
-                            <FontAwesomeIcon icon={faArrowLeft} size={24} color="#494948" />
-                             <Text style={{
-                            fontSize: 18,
-                            fontFamily: Platform.select({
-                                ios: "Barlow-Bold",
-                                android: "Barlow_700Bold"
-                            }),
-                            color: "#494948",
-                            alignSelf: "center",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            textAlign: "center",
-                            display: "flex",
-                            flexGrow: 1,
-                            fontWeight: 600
-                        }}>Wat wordt er uitgeleend?</Text>
-                        </Pressable>
-                    </SafeAreaView>
-                )
+                header: (props) => NewYellowHeader(props, "Wat wordt er uitgeleend?")
             }}/>
 
              <Stack.Screen  name="cats/index"  options={{
-                header: (props) => (
-                    <SafeAreaView edges={["left", "right", "top"]} style={{
-                        width: "100%",
-                        height: 100,
-                        backgroundColor: "#FFEE49",
-                        paddingHorizontal: 20,
-                    }}>
-                        <Pressable style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            alignItems: "center",
-                            flex: 1
-                        }} onPress={() => props.navigation.goBack()}>
-                            <FontAwesomeIcon icon={faArrowLeft} size={24} color="#494948" />
-                             <Text style={{
-                                fontSize: 18,
-                                fontFamily: Platform.select({
-                                    ios: "Barlow-Bold",
-                                    android: "Barlow_700Bold"
-                                }),
-                                color: "#494948",
-                                alignSelf: "center",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                textAlign: "center",
-                                display: "flex",
-                                flexGrow: 1,
-                                fontWeight: 600
-                            }}>Categorieeen</Text>
-                        </Pressable>
-                    </SafeAreaView>
-                )
+                header: (props) => NewYellowHeader(props, "Categorieeen")
             }}/>
         </Stack>
     </>
 }
+
+const NewYellowHeader =  (props: NativeStackHeaderProps, title: string) => (
+    <SafeAreaView edges={["left", "right", "top"]} style={{
+        width: "100%",
+        height: 100,
+        backgroundColor: "#FFEE49",
+        paddingHorizontal: 20,
+    }}>
+        <Pressable style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            flex: 1
+        }} onPress={() => props.navigation.goBack()}>
+            <FontAwesomeIcon icon={faArrowLeft} size={24} color="#494948" />
+                <Text style={{
+                fontSize: 18,
+                fontFamily: Platform.select({
+                    ios: "Barlow-Bold",
+                    android: "Barlow_700Bold"
+                }),
+                color: "#494948",
+                alignSelf: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                display: "flex",
+                flexGrow: 1,
+                fontWeight: 600
+            }}>{title}</Text>
+        </Pressable>
+    </SafeAreaView>
+)
